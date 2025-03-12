150dcb3bccabd28a49c6d0c82d3ee463d90b6330
unknown <lukasz55556e2@gmail.com>
2025-03-12 14:56:08 +0100

    Zrobiona tablica ogarnąć jak mozemy w nia ingerowac

--- Problemn-hetmanow/Problem n-hetmanow/Problem n-hetmanow.cpp
+++ Problemn-hetmanow/Problem n-hetmanow/Problem n-hetmanow.cpp
@@ -1,7 +1,24 @@
 ﻿#include <iostream>
 using namespace std;
-
+const int wiersze=8;
+const int kolumny=8;
 int main()
 {
-    
+    int table[wiersze][kolumny] = {
+        {2,2,2,2,2,2,2,2},
+        {2,2,2,2,2,2,2,2},
+        {2,2,2,2,2,2,2,2},
+        {2,2,2,2,2,2,2,2},
+        {2,2,2,2,2,2,2,2},
+        {2,2,2,2,2,2,2,2},
+        {2,2,2,2,2,2,2,2},
+        {2,2,2,2,2,2,2,2},
+    };
+    for (int i = 0; i < wiersze; i++) {
+        for (int j = 0;j < kolumny;j++) {
+            cout<<table[i][j]<<"\t";
+        }
+        cout << "\n";
+    }
+    return 0;
 }
